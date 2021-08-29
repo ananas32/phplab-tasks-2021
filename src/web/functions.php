eeed161795b97efe35ad19e637cfb9a1e4a29926
Vitaliy.Loshniy <porsche839@gmail.com>
2021-08-29 22:11:02 +0300

    6-web-airports

--- src/web/functions.php
+++ src/web/functions.php
@@ -6,12 +6,53 @@
  *
  * Create a PhpUnit test (GetUniqueFirstLettersTest) which will check this behavior
  *
- * @param  array  $airports
+ * @param array $airports
  * @return string[]
  */
 function getUniqueFirstLetters(array $airports)
 {
-    // put your logic here
 
-    return ['A', 'B', 'C'];
+    $firstLetters = array_unique(array_map(function ($item) {
+        return $item['name']{0};
+    }, $airports));
+
+    sort($firstLetters);
+
+    return $firstLetters;
+}
+
+function url($page, $filter_by_first_letter, $filter_by_state, $sort)
+{
+    $string = '?';
+
+    if ($page && $page !== false) {
+        $string .= '&page=' . $page;
+    }
+
+    if ($filter_by_first_letter) {
+        $string .= "&filter_by_first_letter={$filter_by_first_letter}";
+    }
+
+    if ($filter_by_state) {
+        $string .= "&filter_by_state={$filter_by_state}";
+    }
+
+    if ($sort) {
+        $string .= "&sort={$sort}";
+    }
+
+    return $string;
+}
+
+function sortByUrl($input)
+{
+    return url($_GET['page'], $_GET['filter_by_first_letter'], $_GET['filter_by_state'], $input);
+}
+
+function activePage($page)
+{
+    if ($_GET['page'] == $page || (empty($_GET['page']) && $page === 1)) {
+        return ' active';
+    }
+    return '';
 }
